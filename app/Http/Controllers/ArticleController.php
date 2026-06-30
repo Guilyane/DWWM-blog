6d51efb989976676fb6ba85d13f8f19b77fcbbdb
Guilyane <vederine.guilyane@gmail.com>
2026-06-30 08:05:46 -0400

    add many changes

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ArticleController extends Controller
 {
-    //
+    public function show() : View {
+        return view('articles-list');
+    }
 }
--- database/migrations/2026_06_26_131226_create_articles_table.php
+++ database/migrations/2026_06_26_131226_create_articles_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->string('slug', 255)->unique();
+            $table->longText('content');
+            $table->enum('status', ['DRAFT', 'PUBLISHED'])->default('DRAFT');
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
