12f2813b723cbb17b925035a661a00279e15d8f0
Guilyane <vederine.guilyane@gmail.com>
2026-07-01 11:19:30 -0400

    add missing use

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
     public function index() : View{
         $categories = Category::all();
 
-        return view('categories-list',['categories'=>$categories])
+        return view('categories-list',['categories'=>$categories]);
     }
 }
