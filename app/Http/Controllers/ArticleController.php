a85a3c3015ee3ae252f0c3f395da954570c826cb
Guilyane Védérine <vederine.guilyane@gmail.com>
2026-07-15 12:08:50 -0400

    feat: implement pagination for articles in index method

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,13 +6,20 @@ use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
-
 class ArticleController extends Controller
 {
-    Public function index() : View {
-	    $articles = Article::all();
-        
-        return view('articles-list',['articles'=>$articles]);
+    public function index(): View
+    {
+        // On utilise paginate() pour activer la pagination
+        $articles = Article::with('category')->paginate(10);
 
+        return view('articles-list', compact('articles'));
+    }
+
+    public function details(int $id): View
+    {
+        $article = Article::with(['category', 'user'])->findOrFail($id);
+
+        return view('articles-details', compact('article'));
     }
 }
