cf257ea0098e997dd9efd25a4598becd4694669c
Guilyane Védérine <vederine.guilyane@gmail.com>
2026-07-22 21:00:10 -0400

    feat: add authentication routes for user registration

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('home');
